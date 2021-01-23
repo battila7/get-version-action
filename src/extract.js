a884f16e0c4ded01b5c0cbba647ab10d66e5d47d
Attila Bagossy <bagossyattila@outlook.com>
2021-01-23 16:49:40 +0100

    feat: implemented semver ref extraction

--- src/extract.js
+++ src/extract.js
@@ -1,4 +1,8 @@
+const semver = require('semver')
+
 const REF_SEPARATOR = '/'
+const PRERELEASE_SEPARATOR = '.'
+const BUILD_SEPARATOR = '.'
 
 function extractVersionFromRef (ref) {
   if (!ref) {
@@ -14,8 +18,33 @@ function extractVersionFromRef (ref) {
 
   return {
     version,
-    versionWithoutV
+    versionWithoutV,
+    ...parseSemver(version)
   }
+}
+
+function parseSemver (version) {
+  const sv = semver.parse(version)
+
+  if (!sv) {
+    return {}
+  }
+
+  const result = {
+    major: sv.major.toString(),
+    minor: sv.minor.toString(),
+    patch: sv.patch.toString()
+  }
+
+  if (sv.prerelease.length > 0) {
+    result.prerelease = sv.prerelease.join(PRERELEASE_SEPARATOR)
+  }
+
+  if (sv.build.length > 0) {
+    result.build = sv.build.join(BUILD_SEPARATOR)
+  }
+
+  return result
 }
 
 module.exports = {
